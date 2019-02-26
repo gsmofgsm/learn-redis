4b1977c29673926e4b732e7052183c4374d5255a
Qing Ma <gsmofgsm@hotmail.com>
2019-02-26 23:16:54 +0100

    hashes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,31 @@
 |
 */
 
+use Illuminate\Support\Facades\Redis;
+
 Route::get('/', function () {
-    return view('welcome');
+    
+    // this could be set by an artisan command everytime redis restarts
+
+//    $user1Stats = [
+//        'favorites' => 50,
+//        'watchLaters' => 90,
+//        'completions' => 25
+//    ];
+//
+//    Redis::hmset('user.1.stats', $user1Stats);
+
+    return Redis::hgetall('user.1.stats');
 });
+
+Route::get('user-favorites', function (){
+    Redis::hincrby('user.1.stats', 'favorites', 1);
+    return Redis::hget('user.1.stats', 'favorites');
+});
+
+Route::get('foo', function (){
+    Cache::put('foo', 'bar', 20);
+    return Cache::get('foo');
+});
+
+
